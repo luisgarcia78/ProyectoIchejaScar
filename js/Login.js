94443eebee494d181e5cc92fa140c0dc34becce4
luis <garcia.lsc144@gmail.com>
2023-04-27 15:27:57 -0600

    v1.2

---
+++ js/Login.js
@@ -0,0 +1,17 @@
+const showPasswordBtn = document.getElementById('show-password-btn');
+showPasswordBtn.addEventListener('click', togglePasswordVisibility);
+
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById('pass');
+  const passwordIcon = showPasswordBtn.querySelector('i');
+  
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    passwordIcon.classList.remove('bi bi-eye-slash-fill');
+    passwordIcon.classList.add('bi-eye');
+  } else {
+    passwordInput.type = 'password';
+    passwordIcon.classList.remove('bi-eye');
+    passwordIcon.classList.add('bi bi-eye-slash-fill');
+  }
+}
