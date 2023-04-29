42a4b2d19ac3b2d17ca64547747b266bc7c394a5
luis <garcia.lsc144@gmail.com>
2023-04-28 23:41:11 -0600

    V1.3

--- js/Login.js
+++ js/Login.js
@@ -1,20 +1,11 @@
-const showPasswordBtn = document.getElementById('show-password-btn');
-showPasswordBtn.addEventListener('click', togglePasswordVisibility);
-
 function togglePasswordVisibility() {
-  const passwordInput = document.getElementById('pass');
-  const passwordIcon = showPasswordBtn.querySelector('i');
-  
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    passwordIcon.classList.remove('bi bi-eye-slash-fill');
-    passwordIcon.classList.add('bi-eye');
+  var passwordInput = document.getElementById("password");
+  var toggleButton = document.getElementsByClassName("toggle-password")[0];
+  if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      toggleButton.textContent = "ocultar";
   } else {
-    passwordInput.type = 'password';
-    passwordIcon.classList.remove('bi-eye');
-    passwordIcon.classList.add('bi bi-eye-slash-fill');
+      passwordInput.type = "password";
+      toggleButton.textContent = "mostrar";
   }
 }
-
-const closeButton = document.createElement('i');
-closeButton.classList.add('bi', 'bi-x');
